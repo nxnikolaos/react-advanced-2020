7c73667f05a138a6eaa1e46e716aa255f61a993a
nxnikolaos <nxnatlas@gmail.com>
2023-01-26 12:21:54 +0200

    useEffect 1-2.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/1-useState/birthday-reminder/birthday-reminder";
+import Setup from "./tutorial/2-useEffect/setup/2-useEffect-cleanup";
 function App() {
   return (
     <div className="container">
